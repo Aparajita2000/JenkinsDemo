7382c58e5cc8b1daf64915ea0dde47272309823a
Aparajita2000 <47855502+Aparajita2000@users.noreply.github.com>
2022-11-18 14:21:21 +0530

    Update date.java

--- date.java
+++ date.java
@@ -1,6 +1,6 @@
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime;    
-public class Date {    
+public class date {    
   public static void main(String[] args) {    
    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");  
    LocalDateTime now = LocalDateTime.now(); 
